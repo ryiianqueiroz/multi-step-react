a5e6abaded6d222259d30b05507d3a44fb7c5c87
ryiiianqueiroz <ryanqueiroz2017@gmail.com>
2024-02-02 23:02:44 -0300

    subindo projeto

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/multi-step-react',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/multi-step-react/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
 })
